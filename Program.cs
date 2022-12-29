ac403e875e4308a00cff000c2b98ea04a239aa75
Omar Kadery <omarkadery@protonmail.com>
2022-12-29 14:11:42 -0500

    Basic GET call for steam games

--- Program.cs
+++ Program.cs
@@ -1,6 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
-app.MapGet("/", () => "Hello World!");
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors();
+
+var app = builder.Build();
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+HttpClient client = new HttpClient();
+
+app.MapGet("/games", async (string steamid) => {
+    var steamResponse = await client.GetStringAsync(String.Format("https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={0}&steamid={1}&format=json&include_appinfo=true", SteamTierListServer.ApiConstants.key, steamid));
+    return steamResponse;
+/*    var proxyResponse = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+    proxyResponse.Content = new StringContent(steamResponse);
+
+    return proxyResponse;*/
+
+});
 
 app.Run();
